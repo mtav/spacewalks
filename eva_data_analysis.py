f90df72d1fe578a973d51b30419c7637c66e87e7
mtav <352716+mtav@users.noreply.github.com>
2025-04-03 10:27:03 +0100

    Created functions to read the JSON input and write the CSV output.

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -1,31 +1,44 @@
-# import statements
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# https://data.nasa.gov/resource/eva.json (with modifications)
-input_file = open('eva-data.json', 'r', encoding='utf-8')
-output_file = open('eva-data.csv','w', encoding='utf-8')
-graph_file = 'myplot.png'
+def read_json_to_dataframe(input_file):
+    print(f'Reading JSON file {input_file}')
+    # Read the data from a JSON file into a Pandas dataframe
+    eva_df = pd.read_json(input_file, convert_dates=['date'])
+    eva_df['eva'] = eva_df['eva'].astype(float)
+    # Clean the data by removing any incomplete rows and sort by date
+    eva_df.dropna(axis=0, inplace=True)
+    eva_df.sort_values('date', inplace=True)
+    return eva_df
 
-# read in json file
-eva_df = pd.read_json(input_file, convert_dates=['date'])
-# convert eva column to float
-eva_df['eva'] = eva_df['eva'].astype(float)
-# removing missing values
-eva_df.dropna(axis=0, inplace=True)
-# sort values by date
-eva_df.sort_values('date', inplace=True)
+def write_dataframe_to_csv(df, output_file):
+    print(f'Saving to CSV file {output_file}')
+    # Save dataframe to CSV file for later analysis
+    df.to_csv(output_file, index=False)
 
-# write out data to csv
-eva_df.to_csv(output_file, index=False)
+# Main code
 
-# create new column duration hours
-eva_df['duration_hours'] = eva_df['duration'].str.split(":").apply(lambda x: int(x[0]) + int(x[1])/60)
-eva_df['cumulative_time'] = eva_df['duration_hours'].cumsum()
+print("--START--")
 
-plt.plot(eva_df['date'], eva_df['cumulative_time'], 'ko-')
+input_file = open('./eva-data.json', 'r', encoding='utf-8')
+output_file = open('./eva-data.csv', 'w', encoding='utf-8')
+graph_file = './cumulative_eva_graph.png'
+
+# Read the data from JSON file
+eva_data = read_json_to_dataframe(input_file)
+
+# Convert and export data to CSV file
+write_dataframe_to_csv(eva_data, output_file)
+
+print(f'Plotting cumulative spacewalk duration and saving to {graph_file}')
+# Plot cumulative time spent in space over years
+eva_data['duration_hours'] = eva_data['duration'].str.split(":").apply(lambda x: int(x[0]) + int(x[1])/60)
+eva_data['cumulative_time'] = eva_data['duration_hours'].cumsum()
+plt.plot(eva_data['date'], eva_data['cumulative_time'], 'ko-')
 plt.xlabel('Year')
 plt.ylabel('Total time spent in space to date (hours)')
 plt.tight_layout()
 plt.savefig(graph_file)
 plt.show()
+
+print("--END--")
