d0b6244139cfa3a74350faface493cb22423cabc
mtav <352716+mtav@users.noreply.github.com>
2025-04-03 12:58:25 +0100

    Added test function for calculate_crew_size().
    -ignoring .coverage
    -Added pytest related modules to requirements.txt
    -Fixed main script and ensured it passes all tests.

--- tests/test_eva_data_analysis.py
+++ tests/test_eva_data_analysis.py
@@ -1,5 +1,5 @@
 import pytest
-from eva_data_analysis import text_to_duration
+from eva_data_analysis import text_to_duration, calculate_crew_size
 
 def test_text_to_duration_float():
     """
@@ -18,3 +18,37 @@ def test_text_to_duration_integer():
     actual_result =  text_to_duration("10:00")
     expected_result = 10
     assert actual_result == expected_result
+
+@pytest.mark.parametrize("input_value, expected_result", [
+    ("", None),
+    ("Valentina Tereshkova;", 1),
+    ("Judith Resnik; Sally Ride;", 2),
+])
+def test_calculate_crew_size(input_value, expected_result):
+    """
+    Test that calculate_crew_size returns expected ground truth values
+    for typical crew values
+    """
+    actual_result = calculate_crew_size(input_value)
+    assert actual_result == expected_result
+
+
+    # # Typical value 0
+    # actual_result =  calculate_crew_size("")
+    # expected_result = None
+    # assert actual_result == expected_result
+
+    # # Typical value 1
+    # actual_result =  calculate_crew_size("alice in chains with 'quotes'")
+    # expected_result = 1
+    # assert actual_result == expected_result
+
+    # # Typical value 2
+    # actual_result =  calculate_crew_size("James Van Hoften;Bill Fisher;")
+    # expected_result = 2
+    # assert actual_result == expected_result
+
+    # # Typical value 3
+    # actual_result =  calculate_crew_size("James Van Hoften;Bill Fisher;bob l'eponge")
+    # expected_result = 3
+    # assert actual_result == expected_result
